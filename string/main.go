02d85795c363fe42c4127e3dd9abc420ccaa42da
zhangqiuli <qiuli_zhang@qingsongchou.com>
2021-02-23 14:40:35 +0800

    字符串截取的比较

--- string/main.go
+++ string/main.go
@@ -9,20 +9,10 @@
 package main
 
 import (
-	"fmt"
-	"test/string/src"
 )
 
 func main(){
-	str := "abcdefg"
 
-	//普通str[1:4]
-	a := src.StrSplit(1,4,str)
-	fmt.Println(a)//bcd
 
-	//中文的截取 []rune转换
-	str = "中文的截取abcdefghjk"
-	a = src.ChineseSplit(1,4,str)
-	fmt.Println(a)//文的截
 
 }
