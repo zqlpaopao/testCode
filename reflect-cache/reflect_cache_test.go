3a6d3922949f304ebb2ac5cab84b4ea38596f6ba
code <43371021+zqlpaopao@users.noreply.github.com>
2022-12-15 17:43:49 +0800

    Update reflect_cache_test.go

--- reflect-cache/reflect_cache_test.go
+++ reflect-cache/reflect_cache_test.go
@@ -59,23 +59,6 @@ func BenchmarkPopulateStructUintPtr(b *testing.B){
 	}
 }
 
-func BenchmarkPopulateStructDescriptor(b *testing.B){
-
-	descriptor, err := describeType((*People)(nil))
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		var m People
-		if err := populateStructUintDescriptor(&m,descriptor);err != nil{
-			b.Fatal(err)
-		}
-		if m.Name != "zhangSan"{
-			b.Fatal("name is err")
-		}
-	}
-}
 
 func BenchmarkSimplePopulateStruct(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -155,20 +138,20 @@ func BenchmarkMap2StructOld(b *testing.B) {
 		"float_32_ptr" :&float32Ptr,
 	}
 	for i := 0; i < b.N; i++ {
-		m["str"] = strconv.Itoa(i)
+		m["str"] = strconv.Itoa(i) +"----"
 		var structs Model
 		//pkg.MapToStruct()
 		if err := pkg.MapToStruct(m,&structs,"json");nil != err{
 			b.Fatal(err)
 		}
-		//fmt.Println(structs)
+		//fmt.Println(structs.Str)
 		//fmt.Println(structs.StrPtr)
 		//fmt.Println(structs.StrPt)
 	}
 }
 
-func BenchmarkMap2StructOver(b *testing.B) {
 
+func BenchmarkMap2StructOverPopulateStructDescriptor(b *testing.B){
 	int8Ptr := 18
 	slice :=  []string{
 		"slice1","slice2",
@@ -186,48 +169,95 @@ func BenchmarkMap2StructOver(b *testing.B) {
 		"int_8_ptr" :&int8Ptr,
 		"slice" :slice,
 		"slice_ptr" :&slicePtr,
+		"float_32" :32.00,
+		"float_32_ptr" :&float32Ptr,
+	}
+	m = m
+	descriptor, err := describeType((*Model)(nil))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		var m Model
+		if err := populateStructUintDescriptor(&m,descriptor);err != nil{
+			b.Fatal(err)
+		}
+		if m.Str != "zhangSan"{
+			b.Fatal("name is err")
+		}
+	}
+}
+
+
+func BenchmarkMap2StructOver(b *testing.B) {
+	int8Ptr := 18
+	slice :=  []string{
+		"slice1","slice2",
+	}
+	slicePtr := []string{
+		"slice1Ptr","slice2Ptr",
+	}
+	strPtr := "str_ptrs"
+
+	float32Ptr := 32.01
+
+	var m = map[string]interface{}{
+		"str" :"str",
+		"str_ptr" :&strPtr,
+		"int_8" :int8(8),
+		"int_8_ptr" :&int8Ptr,
+		"slice" :slice,
+		"slice_ptr" :&slicePtr,
 		"float_32" :float32(32.00),
 		"float_32_ptr" :&float32Ptr,
 	}
+	//
+	//m = m
+
+	var tagNames = []*TagName{
+		{
+			StructName: "Str",
+			Type: "string",
+			MapKey:  "str",
+		},
+		{
+			StructName: "Int8",
+			Type: "int8",
+			MapKey:  "int_8",
+		},
+		{
+			StructName: "Slice",
+			Type: "sliceString",
+			MapKey:  "slice",
+		},
+		{
+			StructName: "Float32",
+			Type: "float32",
+			MapKey:  "float_32",
+		},
+		{
+			StructName: "Slice",
+			Type: "sliceString",
+			MapKey:  "slice",
+		},
+
+	}
+
+	b.StartTimer()
+	err := DescribeStructUnsafePointer((*Model)(nil),tagNames)
+	if err != nil {
+		b.Fatal(err)
+	}
 	for i := 0; i < b.N; i++ {
-		m["str"] = strconv.Itoa(i)
+		m["str"] = strconv.Itoa(i+100)
 		var structs Model
 		//pkg.MapToStruct()
-		if err := Map2StructOver(m,&structs,map[string]*tagName{
-			"Str":&tagName{
-				Type: "string",
-				key:  "str",
-			},
-			"Int8":&tagName{
-				Type: "int8",
-				key:  "int_8",
-			},
-			"Slice":&tagName{
-				Type: "sliceStr",
-				key:  "slice",
-			},
-			"Float32":&tagName{
-				Type: "float32",
-				key:  "float_32",
-			},
-			//,"Slice":&tagName{
-			//	Type: "sliceStr",
-			//	key:  "slice",
-			//},
-
-		});nil != err{
+		if err := Map2StructOver(&structs,tagNames,m);nil != err{
 			b.Fatal(err)
 		}
-		//fmt.Println(structs.Slice)
+		//fmt.Println(structs)
 		//fmt.Println(structs.StrPtr)
 		//fmt.Println(structs.StrPt)
 	}
 }
-//
-//Str string `json:"str"`
-////StrPtr *string `json:"str_ptr"`
-//Int8   int8 `json:"int_8"`
-////Int8Ptr *int8 `json:"int_8_ptr"`
-//Slice []string `json:"slice"`
-////SlicePtr *[]string `json:"slice_ptr"`
-//Float32 float32 `json:"float_32"`
